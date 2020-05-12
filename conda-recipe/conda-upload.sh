6dfb22de9424acfaf31db71b8804b25d68c9d2b9
Tara Furstenau <tara.furstenau@nau.edu>
2020-05-11 20:11:31 -0700

    Update conda-upload.sh

--- conda-recipe/conda-upload.sh
+++ conda-recipe/conda-upload.sh
@@ -7,4 +7,4 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION="1.0.1"
 conda build ..
-conda install your-package --use-local
+conda install mtsv-tools --use-local
